c2215c1fafdec015c508758f71a9d63cc4c461f7
Bocanasa, LLC <info@bocanasa.com>
2024-06-25 13:32:49 -0500

    Hardcoded video file name

--- Sources/DecodeFrames/Views/VideoListView.swift
+++ Sources/DecodeFrames/Views/VideoListView.swift
@@ -13,6 +13,9 @@ public struct VideoListView: View {
     @State var videoFilesURL: [String] = FileService.shared.getAllVideoFiles()
     
     public var body: some View {
+        Text("Select the video to decode and preview")
+            .font(.headline)
+        Divider()
         VStack {
             ForEach(videoFilesURL.indices, id: \.self) { index in
                 Text("\(videoFilesURL[index])")
@@ -21,14 +24,16 @@ public struct VideoListView: View {
                         print(videoSelectedName)
                         showPreview = true
                     }
+                    .padding(.all, 10)
             }
             .onAppear() {
                 videoFilesURL = FileService.shared.getAllVideoFiles()
             }
         }
         .fullScreenCover(isPresented: $showPreview, content: {
-            VideoPreview(url: Bundle.main.url(forResource: "Big_Buck_Bunny.mov", withExtension: nil)!)
+            VideoPreview(url: Bundle.main.url(forResource: "frames-7501.h265", withExtension: nil)!)
         })
+        Spacer()
     }
     public init() {}
 }
